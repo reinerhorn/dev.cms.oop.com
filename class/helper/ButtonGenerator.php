8cf3557f36dce7e36ade337471d432bbbfbe7e2d
Reiner Horn <reiner_horn@t-online.de>
2025-08-05 10:13:56 +0700

    Noch offene Änderungen

--- class/helper/ButtonGenerator.php
+++ class/helper/ButtonGenerator.php
@@ -11,5 +11,25 @@ class ButtonGenerator {
             htmlspecialchars($label)
         );
     }
+    public static function renderToggleSwitch(string $name, bool $checked = false, string $onLabel = 'ON', string $offLabel = 'OFF', string $id = '', string ...$extraClasses): string {
+        $id = $id ?: 'toggle_' . uniqid();
+        $classes = implode(' ', array_merge(['toggle-switch'], $extraClasses));
+        return sprintf(
+            '<div class="%s">
+                <input type="checkbox" id="%s" name="%s" %s>
+                <label for="%s" class="switch-label">
+                    <span class="switch-inner" data-on="%s" data-off="%s"></span>
+                    <span class="switch-switch"></span>
+                </label>
+            </div>',
+            htmlspecialchars($classes),
+            htmlspecialchars($id),
+            htmlspecialchars($name),
+            $checked ? 'checked' : '',
+            htmlspecialchars($id),
+            htmlspecialchars($onLabel),
+            htmlspecialchars($offLabel)
+        );
+    }
 }
 ?>
