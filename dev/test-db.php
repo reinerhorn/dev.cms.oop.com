e1c26d8a679835dc943a277e21c263f2f724bff7
Reiner Horn <reiner_horn@t-online.de>
2025-08-09 13:13:52 +0700

    dev zum/dev gelöscht

--- dev/test-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = require_once '/private/conf/h-d_config.php';
-$conn = new mysqli($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name']);
-
-if ($conn->connect_error) {
-    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-}
-echo "Verbindung erfolgreich";
-?>
