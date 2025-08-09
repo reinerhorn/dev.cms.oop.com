5c03a9e262e778e188be663f5011780fb0ac76a4
Reiner Horn <reiner_horn@t-online.de>
2025-08-09 13:57:21 +0700

    smtp_config.php

--- smtp_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host'        => 'securemail.t-online.de',
-    'username'    => 'horn.it@t-online.de',
-    'password'    => '101tanzen101',
-    'smtp_secure' => 'tls',
-    'port'        => 587,
-    'charset'     => 'UTF-8',
-];
